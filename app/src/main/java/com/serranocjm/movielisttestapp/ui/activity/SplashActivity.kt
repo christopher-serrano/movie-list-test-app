7b3bac3dff7622df3c924b6f4225ffa615c57da0
C. Serrano <serrano.cjm@gmail.com>
2023-01-08 09:11:52 -0400

    Screens: - Add splash screen - Code cleanup

---
+++ app/src/main/java/com/serranocjm/movielisttestapp/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.serranocjm.movielisttestapp.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.serranocjm.movielisttestapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}

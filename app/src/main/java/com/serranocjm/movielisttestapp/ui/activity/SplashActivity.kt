b8366dd2ee095e14080ca8233bf0268ed75c90d4
C. Serrano <serrano.cjm@gmail.com>
2023-01-08 10:00:56 -0400

    Refactor activities

--- app/src/main/java/com/serranocjm/movielisttestapp/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/serranocjm/movielisttestapp/ui/activity/SplashActivity.kt
@@ -1,12 +1,18 @@
 package com.serranocjm.movielisttestapp.ui.activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.serranocjm.movielisttestapp.R
+import com.serranocjm.movielisttestapp.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

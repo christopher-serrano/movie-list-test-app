37acac7409c5f87936962e45bf8054fcc4ab69e2
C. Serrano <serrano.cjm@gmail.com>
2023-01-09 11:00:51 -0400

    Movie detail fragment (WIP)

--- app/src/main/java/com/serranocjm/movielisttestapp/ui/fragment/MovieDetailFragment.kt
+++ app/src/main/java/com/serranocjm/movielisttestapp/ui/fragment/MovieDetailFragment.kt
@@ -4,10 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.serranocjm.movielisttestapp.R
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.navArgs
+import com.serranocjm.movielisttestapp.data.remote.model.Movie
+import com.serranocjm.movielisttestapp.databinding.FragmentMovieDetailBinding
+import com.serranocjm.movielisttestapp.ui.viewmodel.MovieViewModel
+import com.serranocjm.movielisttestapp.utils.toastLong
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class MovieDetailFragment : Fragment() {
+class MovieDetailFragment : BaseFragment(), KoinComponent {
+
+    private val viewModel: MovieViewModel by inject()
+
+    private var _binding: FragmentMovieDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: MovieDetailFragmentArgs by navArgs()
+
+    private var movieId: String = ""
+    private var movie: Movie? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +34,39 @@ class MovieDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        observeViewModel()
+        _binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initValues()
+        loadData()
+    }
+
+    override fun initValues() {
+        super.initValues()
+        movieId = args.movieId
+    }
+
+    override fun observeViewModel() = viewModel.run {
+        onError.observe(viewLifecycleOwner) {
+            requireActivity().toastLong(it)
+        }
+        loadingState.observe(viewLifecycleOwner) {
+            binding.clLoading.isVisible = it
+        }
+        movieDetail.observe(viewLifecycleOwner) {
+            movie = it
+        }
+    }
+
+    override fun loadData() {
+        super.loadData()
+        viewModel.getMovieDetail(movieId)
+    }
+
+    private fun setUpFields() {
     }
 }
